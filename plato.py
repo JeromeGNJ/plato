35be2bbaeacb9e1ab69f32622f522c88a9e7acaa
Jerome Paulraj <jeromegnj@gmail.com>
2015-12-20 21:53:03 +0530

    Basic

---
+++ plato.py
@@ -0,0 +1,38 @@
+# Plato
+
+# IMPORTS
+#-----------------------
+import os
+
+
+#FUNCTIONS
+#_______________
+def clr():
+    os.system('cls')
+
+def ent():
+    foo=raw_input()
+
+def nn():
+    print "\n"
+    
+
+#VARIABLES
+#----------------------------
+Questions = {}
+Answers = {}
+Trash = {}
+
+
+#START OF PROGRAM
+#--------------------------------
+
+print 'Plato Interactive Mode'
+nn()
+print 'Plato : Hi !'
+nn()
+
+ent()
+
+clr()
+
